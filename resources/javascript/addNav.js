475e5cc630cc007fe2357c60822c87a16e6c4adb
rcd1 <ryandavis659@gmail.com>
2025-09-21 21:48:09 -0400

    ME LINKS ARE BROKEN

--- resources/javascript/addNav.js
+++ resources/javascript/addNav.js
@@ -3,16 +3,16 @@ addFaviconToPage();
 // Adds a navigation bar to the page
 function addNavigationToPage() {
     let nav = document.getElementById("nav");
-    nav.innerHTML = "<td><a href=\"index.html\" target=\"_parent\">Home</a></td> " +
-    "<td><a href=\"pages\\projects\\projects.html\" target=\"_parent\">Projects</a></td>" +
-    "<td><a href=\"pages\\art\\art.html\" target=\"_parent\">Art</a></td>" +
-    "<td><a href=\"pages\\misc\\misc.html\" target=\"_parent\">Miscellaneous</a></td>";
+    nav.innerHTML = "<td><a href=\"\\index.html\" target=\"_parent\">Home</a></td> " +
+    "<td><a href=\"\\pages\\projects\\projects.html\" target=\"_parent\">Projects</a></td>" +
+    "<td><a href=\"\\pages\\art\\art.html\" target=\"_parent\">Art</a></td>" +
+    "<td><a href=\"\\pages\\misc\\misc.html\" target=\"_parent\">Miscellaneous</a></td>";
 }
 
 function addFaviconToPage() {
     let link = document.createElement("link");
     link.rel = "icon";
-    link.href = "images/cow.gif"; // Path to your favicon
+    link.href = "resources/images/cow.gif"; // Path to your favicon
     link.type = "image/gif"; // Specify the type of the favicon
     document.head.appendChild(link);
 }
